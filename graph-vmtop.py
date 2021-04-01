e4d1e9ea0bb2c976cd2cc8191df8a58eb7815e36
Julien Desfossez <jdesfossez@digitalocean.com>
2021-04-01 08:21:31 -0400

    units and filename
    Signed-off-by: Julien Desfossez <jdesfossez@digitalocean.com>

--- graph-vmtop.py
+++ graph-vmtop.py
@@ -20,6 +20,10 @@ parser.add_argument('-f', '--filename', type=str, required=True,
                     help='csv list of input files to graph')
 parser.add_argument('-m', '--metric', type=str, required=True,
                     help='csv list of metrics to graph')
+parser.add_argument('-u', '--units', type=str, required=True,
+                    help='csv list of units (same order as the metrics)')
+parser.add_argument('-n', '--name', type=str, required=True,
+                    help='Prefix of output file')
 parser.add_argument('-s', '--separate', action='store_true',
                     help='Make 1 chart per metric')
 parser.add_argument('-b', '--begin', type=str,
@@ -35,8 +39,12 @@ if args.end:
     args.end = datetime.datetime.strptime(args.end, "%Y-%m-%d %H:%M:%S")
 
 metrics = ['timestamp']
+i = 0
+units = {}
 for m in args.metric.split(','):
     metrics.append(m)
+    units[m] = args.units.split(',')[i]
+    i += 1
 
 files = {}
 for v in args.filename.split(','):
@@ -66,20 +74,26 @@ if len(metrics) > 2 and not args.separate:
     fig, axs = plt.subplots(len(metrics) - 1, sharex=True)
     if args.title:
         fig.suptitle(args.title)
-    fig.set_figwidth(16)
-    fig.set_figheight(3 * (len(metrics) - 1))
+    h = 4.0 * (len(metrics) - 1)
+    fig.set_figwidth(h * (16.0/9.0))
+    fig.set_figheight(h)
 
     i = 0
     for m in metrics:
         if m == 'timestamp':
             continue
 
-        axs[i].set(xlabel='Time', ylabel=m)
+        axs[i].set(xlabel='Time', ylabel="%s %s" % (m, units[m]))
         for f in files.keys():
             axs[i].plot(files[f]['timestamp'], files[f][m], label=f)
 
         i += 1
-    out_file = os.path.join(args.path, "multi.png")
+    plt.gcf().autofmt_xdate()
+    plt.legend()
+    if args.name is not None:
+        out_file = os.path.join(args.path, "%s.png" % args.name)
+    else:
+        out_file = os.path.join(args.path, "multi.png")
     fig.savefig(out_file)
     plt.close()
     print("Generated %s" % out_file)
@@ -91,13 +105,16 @@ else:
         if args.title:
             fig.suptitle(args.title)
         plt.xlabel('Time')
-        plt.ylabel(m)
+        plt.ylabel("%s %s" % (m, units[m]))
         for f in files.keys():
             plt.plot(files[f]['timestamp'], files[f][m], label=f)
         plt.gcf().autofmt_xdate()
         plt.legend()
 
-        out_file = os.path.join(args.path, "%s.png" % m)
+        if args.name is not None:
+            out_file = os.path.join(args.path, "%s-%s.png" % (args.name, m))
+        else:
+            out_file = os.path.join(args.path, "%s.png" % m)
         fig.savefig(out_file)
         plt.close()
         print("Generated %s" % out_file)

f619e4cd64f00da0e5da8c56aaecbac66eeb8755
Mike Pontillo <mpontillo@digitalocean.com>
2024-04-16 08:26:59 -0700

    reformat code with 'black'

--- extras/core-sched-stats.py
+++ extras/core-sched-stats.py
@@ -16,8 +16,10 @@ try:
     from babeltrace import TraceCollection
 except ImportError:
     # quick fix for debian-based distros
-    sys.path.append("/usr/local/lib/python%d.%d/site-packages" %
-                    (sys.version_info.major, sys.version_info.minor))
+    sys.path.append(
+        "/usr/local/lib/python%d.%d/site-packages"
+        % (sys.version_info.major, sys.version_info.minor)
+    )
     from babeltrace import TraceCollection
 
 
@@ -48,35 +50,35 @@ class Process:
             stats_dict[key] = 0
         stats_dict[key] += co_sched_ns
 
-    def update_co_sched_stats(self, sibling_proc, sibling_tuple, prev_tuple, co_sched_ns):
+    def update_co_sched_stats(
+        self, sibling_proc, sibling_tuple, prev_tuple, co_sched_ns
+    ):
         # Only if we know the neighbor process
         if sibling_proc is None:
             self.unknown_cosched_ns += co_sched_ns
             return
 
         if sibling_proc.pid == 0:
-            self.update_or_create(self.idle_neighbors_stats,
-                                  sibling_tuple,
-                                  co_sched_ns)
+            self.update_or_create(self.idle_neighbors_stats, sibling_tuple, co_sched_ns)
         elif self != sibling_proc:
             if self.pid == 0:
-                sibling_proc.update_or_create(sibling_proc.idle_neighbors_stats,
-                                              prev_tuple,
-                                              co_sched_ns)
+                sibling_proc.update_or_create(
+                    sibling_proc.idle_neighbors_stats, prev_tuple, co_sched_ns
+                )
             else:
-                sibling_proc.update_or_create(sibling_proc.foreign_neighbors_stats,
-                                              prev_tuple,
-                                              co_sched_ns)
-            self.update_or_create(self.foreign_neighbors_stats,
-                                  sibling_tuple,
-                                  co_sched_ns)
+                sibling_proc.update_or_create(
+                    sibling_proc.foreign_neighbors_stats, prev_tuple, co_sched_ns
+                )
+            self.update_or_create(
+                self.foreign_neighbors_stats, sibling_tuple, co_sched_ns
+            )
         elif self == sibling_proc:
-            sibling_proc.update_or_create(sibling_proc.local_neighbors_stats,
-                                          prev_tuple,
-                                          co_sched_ns)
-            self.update_or_create(self.local_neighbors_stats,
-                                  sibling_tuple,
-                                  co_sched_ns)
+            sibling_proc.update_or_create(
+                sibling_proc.local_neighbors_stats, prev_tuple, co_sched_ns
+            )
+            self.update_or_create(
+                self.local_neighbors_stats, sibling_tuple, co_sched_ns
+            )
         else:
             sibling_tid = sibling_tuple[1]
             print("Missing case %s and %s" % (prev_tuple, sibling_tid))
@@ -87,51 +89,68 @@ class Process:
         for neighbor in self.local_neighbors_stats.keys():
             total += self.local_neighbors_stats[neighbor]
             if self.show_details:
-                tmp_list += "    - %s (%s): %s ns\n" % (neighbor[0],
-                                                         neighbor[1],
-                                                         self.local_neighbors_stats[neighbor])
+                tmp_list += "    - %s (%s): %s ns\n" % (
+                    neighbor[0],
+                    neighbor[1],
+                    self.local_neighbors_stats[neighbor],
+                )
         tmp_list += "  - threads: %s\n" % (self.threads)
 
         if self.total_runtime == 0 or total == 0:
             pc = 0
         else:
             pc = total / self.total_runtime * 100
-        co_sched_list = "  - local neighbors (total: %s ns, %0.3f %% of process runtime):\n%s" % (total, pc, tmp_list)
+        co_sched_list = (
+            "  - local neighbors (total: %s ns, %0.3f %% of process runtime):\n%s"
+            % (total, pc, tmp_list)
+        )
 
         tmp_list = ""
         total = 0
         for neighbor in self.idle_neighbors_stats.keys():
             total += self.idle_neighbors_stats[neighbor]
             if self.show_details:
-                tmp_list += "    - %s (%s): %s ns\n" % (neighbor[0],
-                                                         neighbor[1],
-                                                         self.idle_neighbors_stats[neighbor])
+                tmp_list += "    - %s (%s): %s ns\n" % (
+                    neighbor[0],
+                    neighbor[1],
+                    self.idle_neighbors_stats[neighbor],
+                )
         if self.total_runtime == 0 or total == 0:
             pc = 0
         else:
             pc = total / self.total_runtime * 100
-        co_sched_list += "  - idle neighbors (total: %s ns, %0.3f %% of process runtime):\n%s" % (total, pc, tmp_list)
+        co_sched_list += (
+            "  - idle neighbors (total: %s ns, %0.3f %% of process runtime):\n%s"
+            % (total, pc, tmp_list)
+        )
 
         tmp_list = ""
         total = 0
         for neighbor in self.foreign_neighbors_stats.keys():
             total += self.foreign_neighbors_stats[neighbor]
             if self.show_details:
-                tmp_list += "    - %s (%s): %s ns\n" % (neighbor[0],
-                                                         neighbor[1],
-                                                         self.foreign_neighbors_stats[neighbor])
+                tmp_list += "    - %s (%s): %s ns\n" % (
+                    neighbor[0],
+                    neighbor[1],
+                    self.foreign_neighbors_stats[neighbor],
+                )
         if self.total_runtime == 0 or total == 0:
             pc = 0
         else:
             pc = total / self.total_runtime * 100
-        co_sched_list += "  - foreign neighbors (total: %s ns, %0.3f %% of process runtime):\n%s" % (total, pc, tmp_list)
+        co_sched_list += (
+            "  - foreign neighbors (total: %s ns, %0.3f %% of process runtime):\n%s"
+            % (total, pc, tmp_list)
+        )
 
         if self.total_runtime == 0 or self.unknown_cosched_ns == 0:
             pc = 0
         else:
             pc = self.unknown_cosched_ns / self.total_runtime * 100
-        co_sched_list += "  - unknown neighbors  (total: %s ns, %0.3f %% of process runtime)\n" % (
-                self.unknown_cosched_ns, pc)
+        co_sched_list += (
+            "  - unknown neighbors  (total: %s ns, %0.3f %% of process runtime)\n"
+            % (self.unknown_cosched_ns, pc)
+        )
 
         total = 0
         _min = None
@@ -147,25 +166,49 @@ class Process:
                 _max = period
         if len(self.lb_issue_durations) == 0:
             pc = 0
-            co_sched_list += "  - inefficient periods (total: %s ns, %0.3f %% of process runtime)\n" % (total, pc)
+            co_sched_list += (
+                "  - inefficient periods (total: %s ns, %0.3f %% of process runtime)\n"
+                % (total, pc)
+            )
         else:
             pc = total / self.total_runtime * 100
-            co_sched_list += "  - inefficient periods (total: %s ns, %0.3f %% of process runtime):\n" % (total, pc)
-            co_sched_list += "    - number of periods: %s\n" % (len(self.lb_issue_durations))
+            co_sched_list += (
+                "  - inefficient periods (total: %s ns, %0.3f %% of process runtime):\n"
+                % (total, pc)
+            )
+            co_sched_list += "    - number of periods: %s\n" % (
+                len(self.lb_issue_durations)
+            )
             co_sched_list += "    - min period duration: %s ns\n" % (_min)
             co_sched_list += "    - max period duration: %s ns\n" % (_max)
-            co_sched_list += "    - average period duration: %0.3f ns\n" % (total/len(self.lb_issue_durations))
+            co_sched_list += "    - average period duration: %0.3f ns\n" % (
+                total / len(self.lb_issue_durations)
+            )
             if len(self.lb_issue_durations) > 1:
-                co_sched_list += "    - stdev: %0.3f\n" % (statistics.stdev(self.lb_issue_durations))
+                co_sched_list += "    - stdev: %0.3f\n" % (
+                    statistics.stdev(self.lb_issue_durations)
+                )
 
-        return("""Process %s (%s):
+        return """Process %s (%s):
   - total runtime: %s ns,
-%s""" % (self.pid, self.comm, self.total_runtime, co_sched_list))
+%s""" % (
+            self.pid,
+            self.comm,
+            self.total_runtime,
+            co_sched_list,
+        )
 
 
 class TraceParser:
-    def __init__(self, trace, topology_filename, display_pids, show_details,
-                 merge_vhost, pid_trust):
+    def __init__(
+        self,
+        trace,
+        topology_filename,
+        display_pids,
+        show_details,
+        merge_vhost,
+        pid_trust,
+    ):
         self.trace = trace
         # PID to Process
         self.processes = {}
@@ -176,7 +219,7 @@ class TraceParser:
         self.show_details = show_details
         self.merge_vhost = merge_vhost
         self.pid_trust = pid_trust
-        self.vhost_tids = {} # a mapping between vhost processes TIDs and qemu PID
+        self.vhost_tids = {}  # a mapping between vhost processes TIDs and qemu PID
 
         self.parse_topo(topology_filename)
 
@@ -196,15 +239,19 @@ class TraceParser:
         self.siblings = {}
         with open(filename) as f:
             for line in f:
-                id1 = int(line.split(',')[0])
-                id2 = int(line.split(',')[1])
+                id1 = int(line.split(",")[0])
+                id2 = int(line.split(",")[1])
                 self.siblings[id1] = id2
                 self.siblings[id2] = id1
 
     def ns_to_hour_nsec(self, ns):
-        d = time.localtime(ns/NSEC_PER_SEC)
-        return "%02d:%02d:%02d.%09d" % (d.tm_hour, d.tm_min, d.tm_sec,
-                                        ns % NSEC_PER_SEC)
+        d = time.localtime(ns / NSEC_PER_SEC)
+        return "%02d:%02d:%02d.%09d" % (
+            d.tm_hour,
+            d.tm_min,
+            d.tm_sec,
+            ns % NSEC_PER_SEC,
+        )
 
     def parse(self):
         # iterate over all the events
@@ -214,19 +261,19 @@ class TraceParser:
             if not self.ready:
                 # Wait to have seen at least one sched_switch per cpu before starting
                 if event.name == "sched:sched_switch":
-                    next_tuple = (event['next_comm'], event['next_pid'])
-                    self.tuple_by_hwthread[event['cpu_id']] = next_tuple
-                    self.current_cosched_begin_ts[next_tuple] = event['timestamp']
-                    self.tids_last_switch_in_ts[event['next_pid']] = event['timestamp']
+                    next_tuple = (event["next_comm"], event["next_pid"])
+                    self.tuple_by_hwthread[event["cpu_id"]] = next_tuple
+                    self.current_cosched_begin_ts[next_tuple] = event["timestamp"]
+                    self.tids_last_switch_in_ts[event["next_pid"]] = event["timestamp"]
 
-                    if event['cpu_id'] not in self.seen_sched_switch_cpu:
-                        self.seen_sched_switch_cpu.append(event['cpu_id'])
+                    if event["cpu_id"] not in self.seen_sched_switch_cpu:
+                        self.seen_sched_switch_cpu.append(event["cpu_id"])
                     if len(self.seen_sched_switch_cpu) == self.nr_cpus:
                         self.ready = True
                         global begin_ts
-                        begin_ts = event['timestamp']
-#                        print(begin_ts)
-#                        print("Ready: %d (%s events skipped)" % (len(self.seen_sched_switch_cpu), count))
+                        begin_ts = event["timestamp"]
+                    #                        print(begin_ts)
+                    #                        print("Ready: %d (%s events skipped)" % (len(self.seen_sched_switch_cpu), count))
                     continue
 
             method_name = "handle_%s" % event.name.replace(":", "_").replace("+", "_")
@@ -235,19 +282,18 @@ class TraceParser:
                 func = getattr(TraceParser, method_name)
                 func(self, event)
             count += 1
-#            if count == 1000000:
-#                print(self.ns_to_hour_nsec(event.timestamp))
-#                break
+        #            if count == 1000000:
+        #                print(self.ns_to_hour_nsec(event.timestamp))
+        #                break
         global end_ts
-        end_ts = event['timestamp']
+        end_ts = event["timestamp"]
 
-#        print("Trace duration: %s ns (%s events)" % (end_ts - begin_ts, count))
+        #        print("Trace duration: %s ns (%s events)" % (end_ts - begin_ts, count))
 
         for p in self.processes.keys():
             if len(self.display_pids) > 0 and p not in self.display_pids:
                 continue
             print(self.processes[p])
-
 
     # Return a Process from a TID, create it if necessary/possible.
     def get_process_tid(self, tid, pid=None, comm=None):
@@ -257,7 +303,7 @@ class TraceParser:
         # as their qemu process.
         if self.merge_vhost:
             if comm is not None and comm.startswith("vhost-"):
-                qemu_pid = int(comm.split('-')[1])
+                qemu_pid = int(comm.split("-")[1])
                 self.vhost_tids[tid] = qemu_pid
                 if pid is not None:
                     pid = qemu_pid
@@ -321,14 +367,13 @@ class TraceParser:
         if nr_inefficient > 1:
             # starting an inefficient period
             if process.lb_issue_start is None:
-                #print(process.threads, self.tuple_by_hwthread)
+                # print(process.threads, self.tuple_by_hwthread)
                 process.lb_issue_start = now
         else:
             # stopping an inefficient period
             if process.lb_issue_start is not None:
                 process.lb_issue_durations.append(now - process.lb_issue_start)
                 process.lb_issue_start = None
-
 
     def handle_sched_sched_switch(self, event):
         timestamp = event.timestamp
@@ -356,7 +401,9 @@ class TraceParser:
         # SWITCH OUT
         if prev_tuple in self.current_cosched_begin_ts.keys():
             co_sched_ns = timestamp - self.current_cosched_begin_ts[prev_tuple]
-            prev_proc.update_co_sched_stats(sibling_proc, sibling_tuple, prev_tuple, co_sched_ns)
+            prev_proc.update_co_sched_stats(
+                sibling_proc, sibling_tuple, prev_tuple, co_sched_ns
+            )
         if prev_tid in self.tids_last_switch_in_ts.keys():
             prev_proc.total_runtime += timestamp - self.tids_last_switch_in_ts[prev_tid]
 
@@ -372,15 +419,34 @@ class TraceParser:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Trace parser')
-    parser.add_argument('path', metavar="<path/to/trace>", help='Trace path')
-    parser.add_argument('--topology', type=str,
-                        help='text file obtained with for i in /sys/devices/system/cpu/cpu*/topology/thread_siblings_list; do cat $i; done > out.txt',
-                        required=True)
-    parser.add_argument('--pids', type=str, help='Only output this coma-separated list of PIDs')
-    parser.add_argument('--show-details', action="store_true", help='Breakdown the co-scheduling stats by neighbor process')
-    parser.add_argument('--merge-vhost', action="store_true", help='Consider vhost processes as part of the Qemu process (based on the procname)', default=False)
-    parser.add_argument('--no-pid-trust', action="store_true", help='Consider vhost processes as part of the Qemu process (based on the procname)', default=False)
+    parser = argparse.ArgumentParser(description="Trace parser")
+    parser.add_argument("path", metavar="<path/to/trace>", help="Trace path")
+    parser.add_argument(
+        "--topology",
+        type=str,
+        help="text file obtained with for i in /sys/devices/system/cpu/cpu*/topology/thread_siblings_list; do cat $i; done > out.txt",
+        required=True,
+    )
+    parser.add_argument(
+        "--pids", type=str, help="Only output this coma-separated list of PIDs"
+    )
+    parser.add_argument(
+        "--show-details",
+        action="store_true",
+        help="Breakdown the co-scheduling stats by neighbor process",
+    )
+    parser.add_argument(
+        "--merge-vhost",
+        action="store_true",
+        help="Consider vhost processes as part of the Qemu process (based on the procname)",
+        default=False,
+    )
+    parser.add_argument(
+        "--no-pid-trust",
+        action="store_true",
+        help="Consider vhost processes as part of the Qemu process (based on the procname)",
+        default=False,
+    )
 
     args = parser.parse_args()
 
@@ -396,11 +462,17 @@ if __name__ == "__main__":
 
     display_pids = []
     if args.pids is not None:
-        for i in args.pids.split(','):
+        for i in args.pids.split(","):
             display_pids.append(int(i))
 
-    t = TraceParser(traces, args.topology, display_pids, args.show_details,
-                    args.merge_vhost, pid_trust)
+    t = TraceParser(
+        traces,
+        args.topology,
+        display_pids,
+        args.show_details,
+        args.merge_vhost,
+        pid_trust,
+    )
     t.parse()
 
     for h in handle.values():
--- graph-vmtop.py
+++ graph-vmtop.py
@@ -6,30 +6,44 @@ import csv
 import datetime
 import pandas as pd
 import matplotlib
-matplotlib.use('Agg')
+
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 
-parser = argparse.ArgumentParser(description='Graph from vmtop')
+parser = argparse.ArgumentParser(description="Graph from vmtop")
 
-parser.add_argument('-p', '--path', type=str, required=True,
-                   help='folder containing the CSV files produced by vmtop')
-parser.add_argument('-t', '--title', type=str,
-        help='Title of the graph')
-parser.add_argument('-f', '--filename', type=str, required=True,
-                    help='csv list of input files to graph')
-parser.add_argument('-m', '--metric', type=str, required=True,
-                    help='csv list of metrics to graph')
-parser.add_argument('-u', '--units', type=str, required=True,
-                    help='csv list of units (same order as the metrics)')
-parser.add_argument('-n', '--name', type=str, required=True,
-                    help='Prefix of output file')
-parser.add_argument('-s', '--separate', action='store_true',
-                    help='Make 1 chart per metric')
-parser.add_argument('-b', '--begin', type=str,
-                    help='Begin timestamp (%Y-%m-%d %H:%M:%S)')
-parser.add_argument('-e', '--end', type=str,
-                    help='Begin timestamp (%Y-%m-%d %H:%M:%S)')
+parser.add_argument(
+    "-p",
+    "--path",
+    type=str,
+    required=True,
+    help="folder containing the CSV files produced by vmtop",
+)
+parser.add_argument("-t", "--title", type=str, help="Title of the graph")
+parser.add_argument(
+    "-f", "--filename", type=str, required=True, help="csv list of input files to graph"
+)
+parser.add_argument(
+    "-m", "--metric", type=str, required=True, help="csv list of metrics to graph"
+)
+parser.add_argument(
+    "-u",
+    "--units",
+    type=str,
+    required=True,
+    help="csv list of units (same order as the metrics)",
+)
+parser.add_argument(
+    "-n", "--name", type=str, required=True, help="Prefix of output file"
+)
+parser.add_argument(
+    "-s", "--separate", action="store_true", help="Make 1 chart per metric"
+)
+parser.add_argument(
+    "-b", "--begin", type=str, help="Begin timestamp (%Y-%m-%d %H:%M:%S)"
+)
+parser.add_argument("-e", "--end", type=str, help="Begin timestamp (%Y-%m-%d %H:%M:%S)")
 
 args = parser.parse_args()
 
@@ -38,16 +52,16 @@ if args.begin:
 if args.end:
     args.end = datetime.datetime.strptime(args.end, "%Y-%m-%d %H:%M:%S")
 
-metrics = ['timestamp']
+metrics = ["timestamp"]
 i = 0
 units = {}
-for m in args.metric.split(','):
+for m in args.metric.split(","):
     metrics.append(m)
-    units[m] = args.units.split(',')[i]
+    units[m] = args.units.split(",")[i]
     i += 1
 
 files = {}
-for v in args.filename.split(','):
+for v in args.filename.split(","):
     files[v] = {}
     for m in metrics:
         files[v][m] = []
@@ -56,16 +70,16 @@ for v in args.filename.split(','):
 for f in files.keys():
     fpath = os.path.join(args.path, "%s.csv" % f)
 
-    reader = csv.DictReader(open(fpath, 'r'))
+    reader = csv.DictReader(open(fpath, "r"))
     for row in reader:
-        dt = datetime.datetime.strptime(row['timestamp'], "%Y-%m-%d %H:%M:%S")
+        dt = datetime.datetime.strptime(row["timestamp"], "%Y-%m-%d %H:%M:%S")
         if args.begin is not None and dt < args.begin:
             continue
         if args.end is not None and dt > args.end:
             break
-        files[f]['timestamp'].append(dt)
+        files[f]["timestamp"].append(dt)
         for m in metrics:
-            if m == 'timestamp':
+            if m == "timestamp":
                 continue
             files[f][m].append(float(row[m]))
 
@@ -75,17 +89,17 @@ if len(metrics) > 2 and not args.separate:
     if args.title:
         fig.suptitle(args.title)
     h = 4.0 * (len(metrics) - 1)
-    fig.set_figwidth(h * (16.0/9.0))
+    fig.set_figwidth(h * (16.0 / 9.0))
     fig.set_figheight(h)
 
     i = 0
     for m in metrics:
-        if m == 'timestamp':
+        if m == "timestamp":
             continue
 
-        axs[i].set(xlabel='Time', ylabel="%s %s" % (m, units[m]))
+        axs[i].set(xlabel="Time", ylabel="%s %s" % (m, units[m]))
         for f in files.keys():
-            axs[i].plot(files[f]['timestamp'], files[f][m], label=f)
+            axs[i].plot(files[f]["timestamp"], files[f][m], label=f)
 
         i += 1
     plt.gcf().autofmt_xdate()
@@ -99,15 +113,15 @@ if len(metrics) > 2 and not args.separate:
     print("Generated %s" % out_file)
 else:
     for m in metrics:
-        if m == 'timestamp':
+        if m == "timestamp":
             continue
-        fig = plt.figure(figsize=(16,9))
+        fig = plt.figure(figsize=(16, 9))
         if args.title:
             fig.suptitle(args.title)
-        plt.xlabel('Time')
+        plt.xlabel("Time")
         plt.ylabel("%s %s" % (m, units[m]))
         for f in files.keys():
-            plt.plot(files[f]['timestamp'], files[f][m], label=f)
+            plt.plot(files[f]["timestamp"], files[f][m], label=f)
         plt.gcf().autofmt_xdate()
         plt.legend()
 
@@ -118,4 +132,3 @@ else:
         fig.savefig(out_file)
         plt.close()
         print("Generated %s" % out_file)
-
